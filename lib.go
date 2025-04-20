881e86b09ea0dc7f56494f440811b0f49c0ef6ac
mohafarman <mohamadfarman977@gmail.com>
2025-04-20 17:18:41 +0200

    added comments

--- lib.go
+++ lib.go
@@ -1,5 +1,13 @@
+// Package performs simple mathematical functions
+// such as addition and in the future subtraction.
 package workspace_lib
 
+// AddNums adds two integers and returns the result.
+// Inspired by [MathIsFun].
+//
+// It has two parameters: two integers and returns an integer.
+//
+// [MathIsFun]: https://www.mathsisfun.com/numbers/addition.html
 func AddNums(a, b int) int {
 	return a + b
 }
