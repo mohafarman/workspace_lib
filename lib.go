120d872768c882eefcc57413c765bcf555c06b69
mohafarman <mohamadfarman977@gmail.com>
2025-04-20 17:33:47 +0200

    AddNums now generic function

--- lib.go
+++ lib.go
@@ -2,12 +2,23 @@
 // such as addition and in the future subtraction.
 package workspace_lib
 
-// AddNums adds two integers and returns the result.
+import (
+	"golang.org/x/exp/constraints"
+)
+
+// Number type interface that includes both integer and float
+// from the constraint module.
+type Number interface {
+	constraints.Integer | constraints.Float
+}
+
+// AddNums adds two Number interface variables and returns the result as a Number interface.
 // Inspired by [MathIsFun].
 //
-// It has two parameters: two integers and returns an integer.
+// It has two parameters: a and b of type Number and returns a Number type.
+// The Number is used from the constraint module.
 //
 // [MathIsFun]: https://www.mathsisfun.com/numbers/addition.html
-func AddNums(a, b int) int {
+func AddNums[T Number](a, b T) T {
 	return a + b
 }
